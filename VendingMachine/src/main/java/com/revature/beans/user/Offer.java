ab64d593de6b869faf1a31fcab22ff8c3a94992b
Jordy Then <42790442+jordythen@users.noreply.github.com>
2020-05-27 21:58:02 -0400

    changed up the snack offer table?

--- VendingMachine/src/main/java/com/revature/beans/user/Offer.java
+++ VendingMachine/src/main/java/com/revature/beans/user/Offer.java
@@ -38,12 +38,14 @@ public class Offer {
 	private VendingMachine vendingMachine;
 	
 	//ASK SIERRA IF THIS IS CORRECT
-	@OneToMany(fetch=FetchType.EAGER,cascade=CascadeType.ALL)
-	@JoinTable(name="SNACKSOFFER", joinColumns=@JoinColumn(name="offerID"),
+	@ManyToMany(fetch=FetchType.EAGER,cascade=CascadeType.ALL)
+	@JoinTable(name="SNACKOFFER", joinColumns=@JoinColumn(name="offerID"),
 									inverseJoinColumns=@JoinColumn(name="snacksOfInterestID"))
+	
+	//IS THIS ONE CORRECT? OR ^???
 	private Set<Snack> snacksOfInterest;
-	@OneToMany(fetch=FetchType.EAGER,cascade=CascadeType.ALL)
-	@JoinTable(name="SNACKSOFFER", joinColumns=@JoinColumn(name="offerID"),
+	@ManyToMany(fetch=FetchType.EAGER,cascade=CascadeType.ALL)
+	@JoinTable(name="SNACKOFFER", joinColumns=@JoinColumn(name="offerID"),
 									inverseJoinColumns=@JoinColumn(name="snacksToOfferID"))
 	private Set<Snack> snacksToOffer;
 	
