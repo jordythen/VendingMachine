2aa61d92daaff1d63650dac5f2358540f590fdb5
ddwyer <dustindwyer24@gmail.com>
2020-05-28 19:00:41 -0400

    SnackHibernate done-ish

---
+++ VendingMachine/src/main/java/com/revature/services/SnackService.java
@@ -0,0 +1,37 @@
+package com.revature.services;
+
+import java.util.List;
+
+public class SnackService implements GenericService{
+
+	@Override
+	public Integer add(Object t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object getById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void update(Object t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void delete(Object t) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
