bb8e44bf32eafd66281e43f3104091d2953d5a48
Jordy Then <42790442+jordythen@users.noreply.github.com>
2020-05-30 16:57:15 -0400

    michael's changes

---
+++ VendingMachine/src/main/java/com/revature/data/OrderDAO.java
@@ -0,0 +1,23 @@
+package com.revature.data;
+
+import java.util.List;
+
+import com.revature.beans.user.Order;
+
+public interface OrderDAO extends GenericDAO<Order>{
+	
+	//get order by vending machine ID
+	//public Order getOrder(Order order);
+	
+	//get all orders from a vending machine
+	//public List<Order> getAllOrders();
+	
+	//update order
+	//public Order updateOrder(Order order);
+	
+	//delete order
+	//public Order deleteOrder(Order order);
+		
+	
+
+}
---
+++ VendingMachine/src/main/java/com/revature/data/OrderHibernate.java
@@ -0,0 +1,139 @@
+package com.revature.data;
+
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+import com.revature.beans.user.Order;
+import com.revature.utils.HibernateUtil;
+
+public class OrderHibernate implements OrderDAO {
+	
+	private HibernateUtil conn = HibernateUtil.getHibernateUtil();
+	Logger logger = Logger.getLogger(OrderHibernate.class);
+
+	@Override
+	public Integer add(Order t) {
+		logger.trace("adding Order: " + t);
+		Session session = conn.getSession();
+		Transaction transaction = null;
+		
+		try {
+			transaction = session.beginTransaction();
+			session.save(t);
+			transaction.commit();
+			
+		}
+		catch(Exception e) {
+			if(transaction != null) {
+				transaction.rollback();
+				logger.trace("Exception occured when adding order: " + e);
+				logger.trace("Transaction: " + transaction);
+				logger.trace("Order: " + t);
+			}
+		}
+		finally {
+			session.close();
+		}
+		return t.getId();
+	} // end add method
+
+	@Override
+	public Order getById(Integer id) {
+		logger.trace("Getting order by id" + id);
+		Session session = conn.getSession();
+		Order order = session.get(Order.class, id);
+			
+		if(order == null) {
+			logger.trace("Session contains: " + order);
+			
+		}
+		
+		logger.trace(order + " Returned from Session");
+		
+		session.close();
+		return order;
+	} // end getById method
+
+	@Override
+	public List<Order> getAll() {
+		logger.trace("Getting all orders");
+		
+		Session session = conn.getSession();
+		String HQL = "From Order";
+		Query<Order> orderQuery = session.createQuery(HQL,Order.class);
+		List<Order> orderList = orderQuery.getResultList();
+		
+		if (orderList == null) {
+			logger.trace("The List of Orders returned: " + orderList);
+		}
+		
+		
+		logger.trace("Orders in List are: " + orderList);
+		session.close();
+		return orderList;
+	}// end getAll method
+
+	@Override
+	public void update(Order t) {
+		logger.trace("Order being updated: " + t);
+		Session session = conn.getSession();
+		Transaction transaction = null;
+		
+		try {
+			transaction = session.beginTransaction();
+			session.update(t);
+			if(transaction != null) {
+
+				transaction.commit();
+				logger.trace("Order updated with: " + t);
+			}
+			else {
+				logger.trace("There was a problem updating. Transaction contained: " + transaction);
+
+			}
+			
+		}
+		catch(Exception e) {
+			if(transaction != null) {
+				transaction.rollback();
+				logger.trace(e);
+			}
+		}
+		finally {
+			session.close();
+		}
+	}//end update method
+
+	@Override
+	public void delete(Order t) {
+		logger.trace("Deleting: " + t);
+		Session session = conn.getSession();
+		Transaction transaction = null;
+		
+		try {
+			transaction = session.beginTransaction();
+			session.delete(t);
+			if(transaction != null) {
+				transaction.commit();
+				logger.trace(t + " Deleted successfully");
+				
+			}
+			
+		}
+		catch(Exception e) {
+			if(transaction != null) {
+				transaction.rollback();
+				logger.trace("There was a problem with the transaction: " + e);
+			}	
+		}
+		finally {
+			session.close();
+			
+		}
+	}	// end delete method
+
+}// end class
