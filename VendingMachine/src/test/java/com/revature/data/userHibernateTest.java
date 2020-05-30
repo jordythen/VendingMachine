8bae2eadfdb3bd85050eac9c467b9c134271d2e7
Jordy Then <42790442+jordythen@users.noreply.github.com>
2020-05-30 15:51:51 -0400

    added Dustin's junit test

--- VendingMachine/src/test/java/com/revature/data/userHibernateTest.java
+++ VendingMachine/src/test/java/com/revature/data/userHibernateTest.java
@@ -4,58 +4,77 @@ import static org.junit.Assert.*;
 
 import java.util.List;
 
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.revature.beans.user.User;
 
 public class userHibernateTest {
 	static UserHibernate uh = new UserHibernate();
-	
-	@Test
-	public void test() {
-		User u = uh.getByUsernameAndPassword("jordythen", "pass");
-		System.out.println("user is " + u);
-		assertTrue(u.getEmail().contains("jordythen@gmail.com"));
+	static int id;
+	static int num;
+	static int bc;
+	//static String uname=
+	@Before
+	public void setNum() {
+		num=uh.getAll().size();
 	}
-	//ADDING works as of Friday May 29 :)
+	@BeforeClass
+	static public void setup() {
+		bc=uh.getAll().size();
+		User u5 = new User();
+		u5.setFirstName("Jordy");
+		u5.setLastName("Then");
+		u5.setEmail("fordythen@gmail.com");
+		u5.setPassword("pass");
+		
+		u5.setUsername("usertest"+bc);
+		u5.setBalance(1000.00);
+		u5.setVendingMachine(null);
+		u5.setRatingHistory(null);
+		id=uh.add(u5);
+	}
 	@Test
-	public void testAdd() {
+	public void getByUsernameAndPasswordTest() {
+		User u = uh.getByUsernameAndPassword("usertest"+bc, "pass");
+		System.out.println("user is " + u);
+		assertTrue(u.getEmail().contains("fordythen@gmail.com"));
+	}
+//	ADDING works as of Friday May 29 :)
+	@Test
+	public void addTest() {
 		User u = new User();
 		u.setFirstName("Jordy");
 		u.setLastName("Then");
 		u.setEmail("jordythen@gmail.com");
 		u.setPassword("pass");
-		u.setUsername("jordythen");
+		u.setUsername("jordythen"+num);
 		u.setBalance(1000.00);
 		u.setVendingMachine(null);
 		u.setRatingHistory(null);
-		uh.add(u);
+		u.setId(uh.add(u));
 		System.out.println("THIS IS THE ID OF CREATED PERSON: "+u.getId());
 
-		User u2 = uh.getByUsernameAndPassword("jordythen", "pass");
+		User u2 = uh.getByUsernameAndPassword("jordythen"+num, "pass");
 		System.out.println("user is " + u2);
 		assertTrue(u2.getEmail().contains("jordythen@gmail.com"));
 	}
 	
 	@Test
 	public void getByIdTest() {
-		User u = uh.getById(14);
+		User u = uh.getById(id);
 		System.out.println("user is " + u);
-		assertTrue(u.getEmail().contains("jordythen@gmail.com"));
+		assertTrue(u.getEmail().contains("fordythen@gmail.com"));
 	}
 	@Test
 	public void getByAllTest() {
 		List<User> u = uh.getAll();
 		System.out.println(u);
-		//assertTrue(u.getEmail().contains("cmonster@email.com"));
+		assertTrue(u.size()>=1);
 	}
 	
-//	@Test
-//	public void getByIdTest() {
-//		List<User> u = uh.getAll();
-//		System.out.println(u);
-//		//assertTrue(u.getEmail().contains("cmonster@email.com"));
-//	}
+
 	
 
 }
