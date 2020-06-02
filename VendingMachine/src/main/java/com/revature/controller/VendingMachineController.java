a4b550f1642459be6a0df99251b7758bd1bae994
asp <apatton271@gmail.com>
2020-06-02 11:50:26 -0500

    added vendingMachineController, updated vendingMachineService with data retrieval methods from vendingMachineHibernate

---
+++ VendingMachine/src/main/java/com/revature/controller/VendingMachineController.java
@@ -0,0 +1,82 @@
+package com.revature.controller;
+
+import java.util.List;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.revature.beans.snack.Type;
+import com.revature.beans.vendingmachine.VendingMachine;
+import com.revature.services.VendingMachineService;
+
+@RestController
+@CrossOrigin(origins ="Http://localhost:4200", allowCredentials = "true")
+@RequestMapping(path="/vendingMachine")
+public class VendingMachineController {
+	private VendingMachineService vendingService; 
+	public Logger logger = Logger.getLogger(VendingMachineController.class);
+	
+
+	@Autowired
+	public VendingMachineController(VendingMachineService vms) {
+		vendingService = vms;
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<VendingMachine>> getAll(){
+		List<VendingMachine> allVendingMachines = vendingService.getAll();
+		
+		return ResponseEntity.ok(allVendingMachines);
+		
+	}
+	
+	@GetMapping(path = "/{id}")
+	public ResponseEntity<VendingMachine> getVendingMachineById(@PathVariable("id") int id){
+		VendingMachine vm = vendingService.getById(id);
+		
+		if(vm == null) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		return ResponseEntity.ok(vm);
+	}
+	
+	@GetMapping(path = "/{name}")
+	public ResponseEntity<List<VendingMachine>> getVendingMachineByName(@PathVariable("name") String name){
+		
+		List<VendingMachine> vm = vendingService.getByName(name);
+		
+		return ResponseEntity.ok(vm);
+		
+	}
+	
+	@GetMapping(path = "/{type}")
+	public ResponseEntity<List<VendingMachine>> getVendingMachineByType(@PathVariable("type") Type type){
+		
+		List<VendingMachine> vm = vendingService.getByType(type);
+		
+		return ResponseEntity.ok(vm);
+	}
+	
+	@GetMapping(path = "/{popularity/highest")
+	public ResponseEntity<List<VendingMachine>> getHighestRatedMachine(){
+		
+		List<VendingMachine> vm = vendingService.getByHighestPopularity();
+		
+		return ResponseEntity.ok(vm);
+	}
+	
+	@GetMapping(path = "/{popularity/lowest")
+	public ResponseEntity<List<VendingMachine>> getLowestRatedMachine(){
+		
+		List<VendingMachine> vm = vendingService.getByLowestPopularity();
+		
+		return ResponseEntity.ok(vm);
+	}
+}
--- VendingMachine/src/main/java/com/revature/services/VendingMachineService.java
+++ VendingMachine/src/main/java/com/revature/services/VendingMachineService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.revature.beans.snack.Type;
 import com.revature.beans.vendingmachine.VendingMachine;
 import com.revature.data.VendingMachineHibernate;
 
@@ -26,6 +27,7 @@ public class VendingMachineService implements GenericService<VendingMachine> {
 	public VendingMachine getById(Integer id) {
 		return vmh.getById(id);
 	}
+	
 
 	@Override
 	public List<VendingMachine> getAll() {
@@ -41,5 +43,24 @@ public class VendingMachineService implements GenericService<VendingMachine> {
 	public void delete(VendingMachine vm) {
 		vmh.delete(vm);
 	}
+	
+	public List<VendingMachine> getByName(String name) {
+		return vmh.getByName(name);
+	}
+	
+	public List<VendingMachine> getByType(Type t){
+		return vmh.getByType(t);
+		
+	}
+	
+	public List<VendingMachine> getByHighestPopularity(){
+		return vmh.getByPopularityHighest();
+	}
+	
+	public List<VendingMachine> getByLowestPopularity(){
+		return vmh.getByPopularityLowest();
+	}
+	
+	
 
 }
