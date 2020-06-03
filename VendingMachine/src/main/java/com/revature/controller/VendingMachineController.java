4e964282761c444bd85eb2e96ce98ca782426faa
Ryan Gerold <ryan.gerold@gmail.com>
2020-06-03 18:42:14 -0400

    saved project

--- VendingMachine/src/main/java/com/revature/controller/VendingMachineController.java
+++ VendingMachine/src/main/java/com/revature/controller/VendingMachineController.java
@@ -8,6 +8,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -78,5 +80,10 @@ public class VendingMachineController {
 		List<VendingMachine> vm = vendingService.getByLowestPopularity();
 		
 		return ResponseEntity.ok(vm);
+	}
+	
+	@PutMapping(path = "/update")
+	public void updateMachine(VendingMachine vm){
+		vendingService.update(vm);
 	}
 }
