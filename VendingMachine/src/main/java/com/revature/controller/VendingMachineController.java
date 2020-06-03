e73f0a9ce0971b76150923afaa52df528412d279
Jordy Then <42790442+jordythen@users.noreply.github.com>
2020-06-03 19:20:39 -0400

    finished alot of problems

--- VendingMachine/src/main/java/com/revature/controller/VendingMachineController.java
+++ VendingMachine/src/main/java/com/revature/controller/VendingMachineController.java
@@ -2,6 +2,8 @@ package com.revature.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -10,11 +12,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.beans.snack.Type;
+import com.revature.beans.user.User;
 import com.revature.beans.vendingmachine.VendingMachine;
+import com.revature.services.UserService;
 import com.revature.services.VendingMachineService;
 
 @RestController
@@ -22,11 +27,14 @@ import com.revature.services.VendingMachineService;
 @RequestMapping(path="/vendingMachine")
 public class VendingMachineController {
 	private VendingMachineService vendingService; 
+
+	private UserService userv;
 	public Logger logger = Logger.getLogger(VendingMachineController.class);
 	
 
 	@Autowired
-	public VendingMachineController(VendingMachineService vms) {
+	public VendingMachineController(VendingMachineService vms, UserService p) {
+		userv = p;
 		vendingService = vms;
 	}
 	
@@ -84,8 +92,22 @@ public class VendingMachineController {
 		return ResponseEntity.ok(vm);
 	}
 	
+	@SuppressWarnings("unused")
 	@PutMapping(path = "/update")
-	public void updateMachine(VendingMachine vm){
+	public ResponseEntity<VendingMachine> updateMachine(@RequestBody VendingMachine vm, HttpSession session){
+		logger.info("Attempting to update vending machine " + vm.getName());
+		if(vm == null) {
+			return ResponseEntity.noContent().build();
+		}
 		vendingService.update(vm);
+		updateSessionUser(session);
+		return ResponseEntity.ok(vendingService.getById(vm.getId()));
 	}
+	
+	public void updateSessionUser(HttpSession session) {
+		User u=(User) session.getAttribute("user");
+		User u2=userv.getById(u.getId());
+		session.setAttribute("user", u2);
+	}
+	
 }
