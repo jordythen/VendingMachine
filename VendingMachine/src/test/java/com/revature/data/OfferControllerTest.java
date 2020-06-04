18e3bada520d95e9c002670d30027c38cd894662
asp <apatton271@gmail.com>
2020-06-04 10:23:42 -0500

    trying to resolve pom error

---
+++ VendingMachine/src/test/java/com/revature/data/OfferControllerTest.java
@@ -0,0 +1,14 @@
+package com.revature.data;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class OfferControllerTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
